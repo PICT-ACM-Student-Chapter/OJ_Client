6324086b8dcc16143653e7b95b23e5d2dc2ef113
Shubham <shubhamchemate3@gmail.com>
2020-11-17 22:11:33 +0530

    instructions page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.less";
 import Navbar from "./components/Navbars/Navbar";
-import ContestLandingPage from "./pages/ContestLandingPage";
 
 function App() {
 	return (
 		<div className="App" style={{ backgroundColor: "black" }}>
 			<Navbar></Navbar>
-			<ContestLandingPage />
 		</div>
 	);
 }
