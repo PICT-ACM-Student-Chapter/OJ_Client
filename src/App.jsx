9039057986b78b635bd12fcae39cbcf019a8d3d1
Om Tayade <omtayade160901@gmail.com>
2020-11-26 14:59:33 +0530

    For ContestDetails Added axios for api calls but fetching id of contest is not resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.less';
-<<<<<<< HEAD
+
 // import Login from './components/Auth/Login';
 // import Navbar from './components/Navbars/Navbar';
 import ContestDetail from './pages/ContestDetailPage/contestdetailpage.component';
@@ -11,16 +11,8 @@ function App() {
       <ContestDetail/>
       
 
-=======
-import Login from './components/Auth/Login';
-import Navbar from './components/Navbars/Navbar';
-import Contests from './pages/Contests';
-function App() {
-  return (
-    <div className="App" style={{backgroundColor:"black"}}>
-      <Navbar></Navbar>
-      <Contests></Contests>
->>>>>>> 6b8c23a588f871673adb2e778599e4e9d8cc498e
+
+
     </div>
   );
 }
--- src/pages/ContestDetailPage/QuestionCard/QuestionCard.component.jsx
+++ src/pages/ContestDetailPage/QuestionCard/QuestionCard.component.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import {Button, Card} from 'antd';
 import './QuestionCard.styles.css'
 
-const QuestionCard =(props)=>{
-   const {difficulty ,title,SuccessRate,maxScore} = props.contents;
+function QuestionCard (props){
+    
+     
+  
    return (
    <div>
-        <Card  style={{ marginBottom: 16 }} className='card-container'    >
-<div className='cardtitle'><h2>{title}</h2> <Button type="primary" ghost> Solve Challenge </Button> </div>
-<div className='cardcontent'> <p>{difficulty}</p> <p>Max Score:{maxScore}</p> <p>Success Rate:{SuccessRate}</p> </div>
-    </Card>
+         <Card  style={{ marginBottom: 16 }} className='card-container'    >
+          { props.questions.map(question => <div className='cardtitle'><h2>{question.name}</h2> <Button style={{color:"white"}} type="primary" ghost> Solve Challenge </Button> </div>)}
+          { props.questions.map(question => <div className='cardcontent'><p>MaxScore:{question.score}</p>  </div>)}
+
+             {/* <div className='cardcontent'> <p>Max Score:{question.score}</p>  </div> )}  */}
+         </Card>
 
    </div>
   
