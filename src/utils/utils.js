e09f1a6bbab40e662415db96e4c5ac4d5566b7e9
pnshiralkar <pnshiralkar@gmail.com>
2020-12-04 13:58:09 +0530

    Refresh token logic added, routes changed acc to new djoser

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const refresh = localStorage.getItem("refresh-token");
 
-const refreshAuthLogic = failedRequest => axios.post(process.env.REACT_APP_BASE_URL + '/auth/token/refresh', {refresh})
+const refreshAuthLogic = failedRequest => axios.post(process.env.REACT_APP_BASE_URL + '/auth/jwt/refresh', {refresh})
     .then(tokenRefreshResponse => {
         localStorage.setItem('token', tokenRefreshResponse.data.access);
         failedRequest.response.config.headers['Authorization'] = 'Bearer ' + tokenRefreshResponse.data.access;
