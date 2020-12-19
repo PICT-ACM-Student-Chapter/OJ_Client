fda8b898e4f9e8be15411f94a3f90178e4408daa
pnshiralkar <pnshiralkar@gmail.com>
2020-12-19 20:49:07 +0530

    Run Submit API integration done

--- src/components/QuestionPage/RunSubmit.component.jsx
+++ src/components/QuestionPage/RunSubmit.component.jsx
@@ -1,55 +1,29 @@
 import React from 'react'
 import {Button, Card, Input, Popconfirm, Space} from "antd";
-import {CaretRightOutlined, DownOutlined, UpOutlined} from "@ant-design/icons";
+import {CaretRightOutlined, DownOutlined, LoadingOutlined, UpOutlined} from "@ant-design/icons";
 import RunOutputComponent from "./RunOutput.component";
 import SubmitComponent from "./Submit.component";
-
+import axios from "axios";
+import {b64Encode, sleep} from '../../utils/utils'
 
 const {useState} = React
 
-
-//-------------- Hardcoded API Responses for testing ---------------
-
-const submitResponse = {
-    verdicts: [
-        {status: 'AC'},
-        {status: 'RTE'},
-        {status: 'TLE'},
-        {status: 'IN_QUEUE'},
-        {status: 'AC'},
-        {status: 'RTE'},
-        {status: 'TLE'},
-        {status: 'IN_QUEUE'},
-        {status: 'AC'},
-        {status: 'RTE'},
-        {status: 'TLE'},
-        {status: 'IN_QUEUE'},
-    ]
-}
-
-const runOutputResponse = {
-    stderr: `./main.c: In function ‘main’:
-./main.c:6:1: error: expected ‘;’ before ‘}’ token
- }
- ^`,
-    stdout: 'Hello OJ',
-    status: 'AC',
-    exec_time: '1.04s'
-}
-
-
-//-------------- Hardcoded API Responses for testing ends ---------------
+const RUN_INTERVAL = 1  // In secs
+const SUBMIT_INTERVAL = 1  // In secs
 
 
 export default function RunSubmit(props) {
     const [isTerminalOpen, setTerminalOpen] = useState(false);
     const [activeTab, setActiveTab] = useState('input')
+    const [input, setInput] = useState('')
+
     const [outputLoading, setOutputLoading] = useState(false);
     const [output, setOutput] = useState({});
+
     const [submissionLoading, setSubmissionLoading] = useState(false);
     const [submission, setSubmission] = useState({});
     const [testCases, setTestCases] = useState([]);
-    const [passedTestCases, setPassedTestCases] = useState(0);
+
 
     const tabList = [
         {
@@ -64,29 +38,99 @@ export default function RunSubmit(props) {
         }
     ]
 
-    const handleRun = () => {
-        setActiveTab('output')
-        setOutputLoading(true)
-        // Simulate latency
-        setTimeout(() => {
+    const checkRun = async (id) => {
+        const reqConfig = {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        }
+        const res = await axios.get(`${process.env.REACT_APP_BASE_URL}/run/${id}`, reqConfig)
+        console.log(res.data)
+        if (res.data.status === 'IN_QUEUE') {
+            await sleep(RUN_INTERVAL * 1000);
+            checkRun(id)
+        } else {
             setOutputLoading(false)
-            setOutput(runOutputResponse)
-        }, 2000)
+            setOutput(res.data)
+        }
     }
 
-    const handleSubmit = () => {
+    const checkSubmit = async (id) => {
+        const reqConfig = {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        }
+        const res = await axios.get(`${process.env.REACT_APP_BASE_URL}/contests/${props.match.params.contestId}/questions/${props.match.params.questionId}/submit/${id}`, reqConfig)
+        console.log(res.data)
+
+        setSubmission(res.data)
+        setTestCases(res.data.verdicts)
+
+        let passed = 0, totalJudged = 0;
+        for (let i of res.data.verdicts) {
+            if (i.status !== 'IN_QUEUE') {
+                if (i.status === 'AC')
+                    passed++;
+                totalJudged++;
+            }
+        }
+
+        if (totalJudged !== res.data.verdicts.length) {
+            await sleep(SUBMIT_INTERVAL)
+            checkSubmit(id)
+        }
+
+    }
+
+    const handleRun = async () => {
+        setActiveTab('output')
+        setOutputLoading(true)
+
+        const data = {
+            lang_id: 1,
+            code: b64Encode(props.getCode()),
+            stdin: b64Encode(input)
+        }
+        const reqConfig = {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        }
+
+        const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/run`, data, reqConfig)
+        const subId = res.data.id
+
+        checkRun(subId)
+    }
+
+
+    const handleSubmit = async () => {
         setActiveTab('submit')
         setSubmissionLoading(true)
-        setPassedTestCases(0)
-        // Simulate latency
-        setTimeout(() => {
-            setSubmissionLoading(false)
-            setTestCases([{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}])
-            setTimeout(() => {
-                setTestCases(submitResponse.verdicts)
-                setPassedTestCases(3)
-            }, 1500)
-        }, 1200)
+
+        const data = {
+            lang_id: 1,
+            code: b64Encode(props.getCode())
+        }
+        const reqConfig = {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        }
+
+        const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/contests/${props.match.params.contestId}/questions/${props.match.params.questionId}/submit`, data, reqConfig)
+
+        console.log(res.data)
+
+        setSubmissionLoading(false)
+
+        if (res.status !== 201)
+            return
+
+        const subId = res.data.id
+        setTestCases(res.data.verdicts)
+        checkSubmit(subId)
     }
 
     return (
@@ -116,7 +160,10 @@ export default function RunSubmit(props) {
                   className='button-group-card' onTabChange={setActiveTab} activeTabKey={activeTab}
                   extra={
                       <Space style={{position: 'absolute', right: 16, zIndex: 10}}>
-                          <Button onClick={handleRun}><CaretRightOutlined/>Run</Button>
+                          <Button disabled={outputLoading} onClick={handleRun}>
+                              {outputLoading ? <LoadingOutlined/> : <CaretRightOutlined/>}
+                              Run
+                          </Button>
                           <Popconfirm
                               placement="topRight"
                               title={'Are you sure you want to submit?'}
@@ -132,14 +179,14 @@ export default function RunSubmit(props) {
                   }>
                 {activeTab === 'input' &&
                 <div style={{margin: '20px', overflow: 'scroll', height: '31vh'}}>
-                    <Input.TextArea size={'large'} autoSize={{minRows: 5, maxRows: 8}}/>
+                    <Input.TextArea size={'large'} autoSize={{minRows: 5, maxRows: 8}}
+                                    onChange={e => setInput(e.target.value)} value={input}/>
                 </div>
                 }
                 {activeTab === 'output' &&
                 <RunOutputComponent output={output} outputLoading={outputLoading}/>}
                 {activeTab === 'submit' &&
-                <SubmitComponent testCases={testCases} submissionLoading={submissionLoading}
-                                                            passedTestCases={passedTestCases}/>}
+                <SubmitComponent testCases={testCases} submissionLoading={submissionLoading} />}
             </Card>}
         </div>
     )
--- src/components/QuestionPage/Submit.component.jsx
+++ src/components/QuestionPage/Submit.component.jsx
@@ -2,14 +2,25 @@ import React from 'react';
 import {Card, Col, Progress, Row, Skeleton, Tag, Typography} from "antd";
 import {CheckCircleOutlined, ClockCircleOutlined, CloseCircleOutlined} from "@ant-design/icons";
 
+
 const SubmitComponent = (props) => {
-    const {testCases, submissionLoading, passedTestCases} = props;
+    const {testCases, submissionLoading} = props;
 
     const testCaseStatusColor = {
         'AC': 'green',
-        'CTE': 'red',
+        'CE': 'red',
+        'WA': 'red',
         'RTE': 'red',
         'TLE': 'orange'
+    }
+
+    let passed = 0, totalJudged = 0;
+    for (let i of props.testCases) {
+        if (i.status !== 'IN_QUEUE') {
+            if (i.status === 'AC')
+                passed++;
+            totalJudged++;
+        }
     }
 
     return (
@@ -20,23 +31,24 @@ const SubmitComponent = (props) => {
                     submit the solution first</Typography.Title></div>}
                 {submissionLoading && <Skeleton active/>}
                 {testCases.length > 0 && !submissionLoading && <div>
-                    <Progress success={{percent: Math.round(passedTestCases / testCases.length * 100, 2)}}
-                              percent={75} strokeColor={'grey'} status={"active"}/>
-                    <Typography.Link>{passedTestCases} of {testCases.length} Test Cases passed</Typography.Link>
+                    <Progress success={{percent: Math.round(passed / testCases.length * 100, 2)}}
+                              percent={Math.round(totalJudged / testCases.length * 100)} strokeColor={'grey'}
+                              status={totalJudged !== testCases.length && "active"}/>
+                    <Typography.Link>{passed} of {testCases.length} Test Cases passed</Typography.Link>
                     <br/><br/>
                     <Row gutter={[32, 32]} justify="center" style={{justifyContent: "center"}}>
                         {testCases.map((tc, i) => (
-                            <Col key={tc.id} span={8}>
-                                <Card style={{height: '5.7rem'}}>
+                            <Col key={tc.test_case}>
+                                <Card style={{height: '5.7rem', padding: '0 3rem 5rem 1rem'}}>
                                     <Skeleton loading={!tc.status || tc.status === 'IN_QUEUE'} round avatar
-                                              paragraph={{rows: 0}} title={{width: '10rem'}} active>
+                                              paragraph={{rows: 0}} title={{width: '5rem'}} active>
                                         <Card.Meta title={`Testcase #${i + 1}`}
                                                    description={<Tag
                                                        color={testCaseStatusColor[tc.status]}>{tc.status}</Tag>}
                                                    avatar={<>
                                                        {tc.status === 'AC' && <Typography.Title level={2}
                                                                                                 type={'success'}><CheckCircleOutlined/></Typography.Title>}
-                                                       {(tc.status === 'CTE' || tc.status === 'RTE') &&
+                                                       {(tc.status === 'CE' || tc.status === 'RTE' || tc.status === 'WA') &&
                                                        <Typography.Title level={2}
                                                                          type={'danger'}><CloseCircleOutlined/></Typography.Title>}
                                                        {tc.status === 'TLE' && <Typography.Title level={2}
