8e44095a28b9eb327f6766f421a99c1a2aabd785
AVC0706 <atharvvijay4@gmail.com>
2020-12-22 21:35:56 +0530

    Contest Detail page done

--- src/pages/ContestDetailPage/ContestDetail.page.jsx
+++ src/pages/ContestDetailPage/ContestDetail.page.jsx
@@ -1,70 +1,156 @@
-import React from 'react'
-import {Card, Col, Row, Statistic, Tabs, Typography} from 'antd';
+import React, {useEffect, useState} from 'react'
+import {Button, Card, Col, Row, Statistic, Tabs, Tag, Typography} from 'antd';
 import {HourglassOutlined} from '@ant-design/icons'
+import axios from "axios";
+import {useParams} from "react-router";
+import ReactMarkdown from 'react-markdown'
+import gfm from 'remark-gfm'
+import ProSkeleton from '@ant-design/pro-skeleton';
+import {Link} from "react-router-dom";
 
 const {TabPane} = Tabs;
 const {Countdown} = Statistic;
 const {Title} = Typography
 
-const deadline = Date.now() + 1000 * 60 * 60 * 24 * 2 + 1000 * 30; // Moment is also OK
-
-const ContestDetail = () => {
-    // let {contestId} = useParams();
-    // const [isLoading, setIsLoading] = useState(false)
+// const deadline = Date.now() + 1000 * 60 * 60 * 24 * 2 + 1000 * 30; // Moment is also OK
 
 
-    // useEffect(() => {
-    //     axios.get(`${process.env.REACT_APP_BASE_URL}/contests/${contestId}`, {
-    //         headers: {
-    //             'Authorization': `Bearer ${localStorage.getItem('token')}`
-    //         }
-    //     }).then(
-    //         (res) => {
-    //             console.log(res.data)
-    //         }
-    //     )
-    //         .then(() => {
-    //             setIsLoading(false)
-    //         })
-    // }, [])
+const ContestDetail = (props) => {
+    let {contestId} = useParams();
+    const [isLoading, setIsLoading] = useState(true)
+    const [contest, setContest] = useState(null)
+
+    useEffect(() => {
+        getContestDetail()
+        // eslint-disable-next-line
+    }, [])
+
+    const getContestDetail = () => {
+        axios.get(`${process.env.REACT_APP_BASE_URL}/contests/${contestId}`, {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        }).then(
+            (res) => {
+                console.log(res.data)
+                setContest(res.data)
+            }
+        )
+            .then(() => {
+                setIsLoading(false)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
     return (
+        isLoading ?
+            <ProSkeleton key={1}/> :
 
-        <div>
-            <Row>
-                <Col span={12}>
-                    <Title>Pratik Daigavane</Title>
-                </Col>
-                <Col>
-                    <Card>
-                        <Countdown prefix={<HourglassOutlined/>} title="Time Left" value={deadline}/>
+            <div style={{'padding': '4%'}}>
+                <Row>
+                    <Col span={12}>
+                        <Title>{contest.name}</Title>
+                    </Col>
+                    <Col>
+                        <Card>
+                            <Countdown prefix={<HourglassOutlined/>} title="Time Left" value={contest.end_time}/>
 
-                    </Card>
+                        </Card>
+                        <br/>
+                        <Button type="primary" size='large' onClick={() => {
+                            props.history.push(`/leaderboard/${contestId}`)
+                        }}>
+                            Leaderboard
+                        </Button>
+                    </Col>
+                </Row>
+                <Row gutter={32}>
+                    <Col xs={24} sm={24} md={24} lg={16} xl={16}>
 
-                </Col>
-            </Row>
-            <Row>
-                <Col xs={24} sm={24} md={24} lg={12} xl={12}>
+                        <Tabs size='large' type="card">
+                            <TabPane tab="Questions" key="1" style={{'padding': '4%'}}>
 
-                    <Tabs size='large' type="card">
-                        <TabPane tab="Questions" key="1">
-                            Content of Tab Pane 1
-                        </TabPane>
-                        <TabPane tab="Instructions" key="2">
-                            Content of Tab Pane 2
-                        </TabPane>
-                    </Tabs>
-                </Col>
-                <Col xs={24} sm={24} md={24} lg={12} xl={12}>
-                    <Title level={2}>Leaderboard</Title>
-                    {/*<Card title='Leaderboard'>*/}
+                                {contest.questions.map((ques) => (
+                                    <Row key={ques.question.id} align="middle" justify="center">
+                                        <Col span={24}>
+                                            <Card bordered={false}>
+                                                <Row align="middle">
+                                                    <Col lg={16} onClick={() => {
+                                                        props.history.push(`/contests/${contestId}/${ques.question.id}`)
+                                                    }}>
+                                                        <h2><Link
+                                                            to={`/contests/${contestId}/${ques.question.id}`}> {ques.question.name}</Link>
+                                                        </h2>
+                                                    </Col>
+                                                    <Col lg={4}>
+                                                        <Tag color='blue' style={{'fontSize': 'larger'}}>
+                                                            Score: {ques.question.score}
+                                                        </Tag>
+                                                    </Col>
+                                                    <Col lg={4}>
+                                                        <Button size='large' onClick={() => {
+                                                            props.history.push(`/contests/${contestId}/${ques.question.id}/submissions`)
+                                                        }}>
+                                                            Submissions
+                                                        </Button>
+                                                    </Col>
+                                                </Row>
+                                            </Card>
+                                        </Col>
+                                        <br/>
+                                    </Row>
+                                ))}
 
-                    {/*</Card>*/}
 
-                </Col>
-            </Row>
-        </div>
+                            </TabPane>
+                            <TabPane tab="Instructions" key="2" style={{'padding': '4%'}}>
+                                <h2><ReactMarkdown plugins={[gfm]} children={contest.instructions}/></h2>
+
+                            </TabPane>
+                        </Tabs>
+                    </Col>
+                    <Col xs={24} sm={24} md={24} lg={8} xl={8}>
+                        <Title level={2}>Something goes here</Title>
+
+
+                    </Col>
+                </Row>
+            </div>
     )
 }
 
 
 export default ContestDetail
+
+
+// const children = {
+//     guide: `# React App for Online Judge
+// [![Netlify Status](https://api.netlify.com/api/v1/badges/055ae047-fa35-42db-a7b9-b55be8743512/deploy-status)](https://app.netlify.com/sites/fervent-bhabha-03f267/deploys)
+//
+// 🔥 Online Judge Platform of PICT ACM Student Chapter
+//
+// ## Guide to use the repo
+//
+// After you clone the repo you need to install necessary node modules, use below command to download them :
+//
+// ### \`yarn install\` or \`npm install\`
+//
+// To start the react server you need to run following command :
+//
+// ### \`yarn start\` or \`npm start\`
+//
+// This runs the app in the development mode.\\
+// Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+//
+// The page will reload if you make edits.\\
+// You will also see any lint errors in the console.
+//
+// ## CI/CD pipeline
+//
+// Main branch is configured to automatically deploy on netlify. You can visit the domain below to access deployed website.
+//
+// ### \`https://fervent-bhabha-03f267.netlify.app/\` or \`www.onlinejudge.ml\`
+// .`
+// }
