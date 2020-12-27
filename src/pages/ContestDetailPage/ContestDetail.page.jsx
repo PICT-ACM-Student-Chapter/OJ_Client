48de3f6d40579412cf88e9dbf6f3c18ad617645d
pnshiralkar <pnshiralkar@gmail.com>
2020-12-27 20:21:02 +0530

    ContestDetail page title update

--- src/pages/ContestDetailPage/ContestDetail.page.jsx
+++ src/pages/ContestDetailPage/ContestDetail.page.jsx
@@ -10,7 +10,7 @@ import StartContestComponent from "../../components/ContestDetailPage/StartConte
 import MiniLeaderBoard from "../../components/ContestDetailPage/MiniLeaderBoard";
 import '../../components/ContestDetailPage/ContestDetail.css'
 import MarkdownMathJaxComponent from "../../components/MarkdownMathJax.component";
-import {parseDate} from "../../utils/utils";
+import {Helmet} from "react-helmet";
 
 const {TabPane} = Tabs;
 const {Countdown} = Statistic;
@@ -57,124 +57,136 @@ const ContestDetail = (props) => {
     }
 
     return (
-        isLoading ?
-            <ProSkeleton key={1}/> :
+        <>
+            <Helmet>
+                <meta charSet="utf-8"/>
+                <title>{`${isLoading ? 'Loading Contest...' : contest.name} - PASC OJ`}</title>
+                <link rel="canonical" href="http://mysite.com/example"/>
+            </Helmet>
+            {isLoading ?
+                <ProSkeleton key={1}/> :
 
-            <div style={{'padding': '2% 4%'}}>
-                <Row gutter={[0, 24]}>
-                    <Col span={12}>
-                        <Title>{contest.name}</Title>
-                    </Col>
-                    <Col span={12} align='right'>
-                        <Row align='right'>
-                            <Col span={16}>
-                                <Card style={{width: '12rem'}} bodyStyle={{padding: '12px 24px'}}>
-                                    <div className={'ant-statistic'}>
-                                        <div className={'ant-statistic-title'}>
-                                            Current Rank
-                                        </div>
-                                        <div className={'ant-statistic-content'}>
-                                            <div className={'ant-statistic-content-prefix'} style={{marginRight: '1rem'}}>
-                                                <TrophyOutlined />
+                <div style={{'padding': '2% 4%'}}>
+                    <Row gutter={[0, 24]}>
+                        <Col span={12}>
+                            <Title>{contest.name}</Title>
+                        </Col>
+                        <Col span={12} align='right'>
+                            <Row align='right'>
+                                <Col span={16}>
+                                    <Card style={{width: '12rem'}} bodyStyle={{padding: '12px 24px'}}>
+                                        <div className={'ant-statistic'}>
+                                            <div className={'ant-statistic-title'}>
+                                                Current Rank
                                             </div>
-                                            <div className={'ant-statistic-content-value'}>
-                                                {currentRank}
+                                            <div className={'ant-statistic-content'}>
+                                                <div className={'ant-statistic-content-prefix'}
+                                                     style={{marginRight: '1rem'}}>
+                                                    <TrophyOutlined/>
+                                                </div>
+                                                <div className={'ant-statistic-content-value'}>
+                                                    {currentRank}
+                                                </div>
                                             </div>
+                                            Score: {contest.user_score}/{contest.max_score}
                                         </div>
-                                        Score: {contest.user_score}/{contest.max_score}
-                                    </div>
-                                </Card>
+                                    </Card>
+                                </Col>
+                                <Col span={8}>
+                                    <Card style={{width: '12rem'}} bodyStyle={{padding: '12px 24px'}}>
+                                        <Countdown prefix={<HourglassOutlined/>} title="Time Left"
+                                                   value={contest.end_time}/>
+                                        Ends: {new Date(contest.end_time).toLocaleTimeString()}
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                    <br/>
+                    {/*<Button type="primary" size='large' onClick={() => {*/
+                    }
+                    {/*    props.history.push(`/leaderboard/${contestId}`)*/
+                    }
+                    {/*}}>*/
+                    }
+                    {/*    Leaderboard*/
+                    }
+                    {/*</Button>*/
+                    }
+
+                    {
+                        !started && <StartContestComponent contest={contest} startContest={startContest}/>
+                    }
+                    {
+                        started && <><Row gutter={32}>
+                            <Col xs={23} sm={23} md={23} lg={15} xl={15}>
+
+                                <Tabs size='large' type="card">
+                                    <TabPane tab="Questions" key="1" style={{'padding': '4%'}}>
+
+                                        {contest.questions.map((ques) => (
+                                            <Row key={ques.question.id} gutter={[0, 18]} align="middle"
+                                                 justify="center">
+                                                <Col span={24}>
+                                                    <Card bordered={false}>
+                                                        <Row align="middle">
+                                                            <Col lg={12} onClick={() => {
+                                                                props.history.push(`/contests/${contestId}/${ques.question.id}`)
+                                                            }}>
+                                                                <h2><Link
+                                                                    to={`/contests/${contestId}/${ques.question.id}`}> {ques.question.name}</Link>
+                                                                </h2>
+                                                            </Col>
+                                                            <Col lg={12} align={'right'}>
+                                                                <Space size={'middle'}>
+                                                                    <Tag color='blue'
+                                                                         style={{
+                                                                             'fontSize': 'larger',
+                                                                             padding: '0.4rem'
+                                                                         }}>
+                                                                        Score: {ques.question.score}
+                                                                    </Tag>
+                                                                    <Link
+                                                                        to={`/contests/${contestId}/${ques.question.id}`}>
+                                                                        <Button size='large' type={'primary'}>
+                                                                            Solve
+                                                                        </Button>
+                                                                    </Link>
+                                                                    <Link
+                                                                        to={`/contests/${contestId}/${ques.question.id}/submissions?name=${ques.question.name}`}>
+                                                                        <Button size='large'>
+                                                                            My Submissions
+                                                                        </Button>
+                                                                    </Link>
+                                                                </Space>
+                                                            </Col>
+                                                        </Row>
+                                                    </Card>
+                                                </Col>
+                                                <br/>
+                                            </Row>
+                                        ))}
+
+
+                                    </TabPane>
+                                    <TabPane tab="Instructions" key="2" style={{'padding': '4%'}}>
+                                        <div style={{fontSize: 'medium'}} id={'instruction-wrapper'}>
+                                            <MarkdownMathJaxComponent
+                                                plugins={[gfm]} children={contest.instructions}/></div>
+
+                                    </TabPane>
+                                </Tabs>
                             </Col>
-                            <Col span={8}>
-                                <Card style={{width: '12rem'}} bodyStyle={{padding: '12px 24px'}}>
-                                    <Countdown prefix={<HourglassOutlined/>} title="Time Left"
-                                               value={contest.end_time}/>
-                                               Ends: {new Date(contest.end_time).toLocaleTimeString()}
-                                </Card>
+                            <Col lg={1}>
+                                <Divider type="vertical" style={{height: "100%"}}/>
                             </Col>
-                        </Row>
-                    </Col>
-                </Row>
-                <br/>
-                {/*<Button type="primary" size='large' onClick={() => {*/
-                }
-                {/*    props.history.push(`/leaderboard/${contestId}`)*/
-                }
-                {/*}}>*/
-                }
-                {/*    Leaderboard*/
-                }
-                {/*</Button>*/
-                }
-
-                {
-                    !started && <StartContestComponent contest={contest} startContest={startContest}/>
-                }
-                {
-                    started && <><Row gutter={32}>
-                        <Col xs={23} sm={23} md={23} lg={15} xl={15}>
-
-                            <Tabs size='large' type="card">
-                                <TabPane tab="Questions" key="1" style={{'padding': '4%'}}>
-
-                                    {contest.questions.map((ques) => (
-                                        <Row key={ques.question.id} gutter={[0, 18]} align="middle" justify="center">
-                                            <Col span={24}>
-                                                <Card bordered={false}>
-                                                    <Row align="middle">
-                                                        <Col lg={12} onClick={() => {
-                                                            props.history.push(`/contests/${contestId}/${ques.question.id}`)
-                                                        }}>
-                                                            <h2><Link
-                                                                to={`/contests/${contestId}/${ques.question.id}`}> {ques.question.name}</Link>
-                                                            </h2>
-                                                        </Col>
-                                                        <Col lg={12} align={'right'}>
-                                                            <Space size={'middle'}>
-                                                                <Tag color='blue'
-                                                                     style={{'fontSize': 'larger', padding: '0.4rem'}}>
-                                                                    Score: {ques.question.score}
-                                                                </Tag>
-                                                                <Link
-                                                                    to={`/contests/${contestId}/${ques.question.id}`}>
-                                                                    <Button size='large' type={'primary'}>
-                                                                        Solve
-                                                                    </Button>
-                                                                </Link>
-                                                                <Link
-                                                                    to={`/contests/${contestId}/${ques.question.id}/submissions?name=${ques.question.name}`}>
-                                                                    <Button size='large'>
-                                                                        My Submissions
-                                                                    </Button>
-                                                                </Link>
-                                                            </Space>
-                                                        </Col>
-                                                    </Row>
-                                                </Card>
-                                            </Col>
-                                            <br/>
-                                        </Row>
-                                    ))}
-
-
-                                </TabPane>
-                                <TabPane tab="Instructions" key="2" style={{'padding': '4%'}}>
-                                    <div style={{fontSize: 'medium'}} id={'instruction-wrapper'}>
-                                        <MarkdownMathJaxComponent
-                                            plugins={[gfm]} children={contest.instructions}/></div>
-
-                                </TabPane>
-                            </Tabs>
-                        </Col>
-                        <Col lg={1}>
-                            <Divider type="vertical" style={{height: "100%"}}/>
-                        </Col>
-                        <Col xs={24} sm={24} md={24} lg={8} xl={8}>
-                            <MiniLeaderBoard match={props.match}/>
-                        </Col>
-                    </Row></>
-                }
-            </div>
+                            <Col xs={24} sm={24} md={24} lg={8} xl={8}>
+                                <MiniLeaderBoard match={props.match}/>
+                            </Col>
+                        </Row></>
+                    }
+                </div>}
+        </>
     )
 }
 
