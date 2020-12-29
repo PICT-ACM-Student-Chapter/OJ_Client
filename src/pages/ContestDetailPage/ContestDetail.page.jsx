523d78c09bcd5466438fa0383733a02e11d6f2e1
pratikdaigavane <daigavanep@gmail.com>
2020-12-29 22:22:46 +0530

    footer visibility improved in darkmode, markdown css

--- src/pages/ContestDetailPage/ContestDetail.page.jsx
+++ src/pages/ContestDetailPage/ContestDetail.page.jsx
@@ -195,6 +195,7 @@ const ContestDetail = (props) => {
                                     <TabPane tab="Instructions" key="2" style={{'padding': '4%'}}>
                                         <div style={{fontSize: 'medium'}} id={'instruction-wrapper'}>
                                             <MarkdownMathJaxComponent
+                                                className="instructions"
                                                 plugins={[gfm]} children={contest.instructions}/></div>
 
                                     </TabPane>
